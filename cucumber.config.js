eb018d8079ef033e80d88567f9e7b965d38855c3
yadavareddy <yadava.lingam@telus.com>
2025-01-21 21:08:31 -0800

    mmmm

--- cucumber.config.js
+++ cucumber.config.js
@@ -1,8 +1,8 @@
-import { setDefaultTimeout } from '@cucumber/cucumber';
-
-setDefaultTimeout(60 * 1000); // Set a timeout of 60 seconds
-
-export default {
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const cucumber_1 = require("@cucumber/cucumber");
+(0, cucumber_1.setDefaultTimeout)(60 * 1000); // Set a timeout of 60 seconds
+exports.default = {
     paths: ['features/**/*.feature'], // Path to feature files
     require: ['features/step_definitions/*.ts'], // Path to step definitions
     format: ['html:reports/cucumber_report.html'], // Optional: for HTML report generation
